57db5f99978b79e9155a2b2a07c2d1cbec3165f1
Roshan Kumar Sharma <rksharma23082002@gmail.com>
2024-10-17 20:03:57 +0530

    Updated cors policy

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://quicklistclient-opdwxcbe1-rksharma23s-projects.vercel.app',
     credentials: true
 }))     //THIS IS TO ALLOW THE CROSS ORIGIN ACCESS AND COMES UNDER THE CORS POLICY
 
